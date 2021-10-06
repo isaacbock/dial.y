6b18af79438b0a1a190ce38f9455fe193d9ec960
Isaac Bock <isaactbock@wustl.edu>
2021-10-06 16:20:16 -0500

    Firebase application credentials

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 });
