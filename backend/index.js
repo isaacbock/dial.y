1448b483baf34231837f06f2fb5915a74f857a08
Isaac Bock <isaactbock@wustl.edu>
2021-10-19 03:44:38 -0500

    Alpha Revision 1: Remove Faulty Voice Commands

--- backend/index.js
+++ backend/index.js
@@ -138,30 +138,21 @@ app.post("/promptListener", async (req, res) => {
 				method: "POST",
 				numDigits: "1",
 				timeout: "6",
-				speechTimeout: "5",
-				input: "speech dtmf",
-				speechModel: "numbers_and_commands",
-				hints: "respond, repeat, cancel",
+				input: "dtmf",
 			});
-			gather.say("To start recording your response, say Respond, or press 1.");
-			gather.say("To repeat their question again, say Repeat, or press 2.");
-			gather.say(
-				"To hang up without recording a response, say Cancel, or press 3."
-			);
+			gather.say("To start recording your response, press 1.");
+			gather.say("To repeat their question again, press 2.");
+			gather.say("To hang up without recording a response, press 3.");
 			gather.pause({ length: 5 });
 
-			gather.say("To start recording your response, say Respond, or press 1.");
-			gather.say("To repeat their question again, say Repeat, or press 2.");
-			gather.say(
-				"To hang up without recording a response, say Cancel, or press 3."
-			);
+			gather.say("To start recording your response, press 1.");
+			gather.say("To repeat their question again, press 2.");
+			gather.say("To hang up without recording a response, press 3.");
 			gather.pause({ length: 10 });
 
-			gather.say("To start recording your response, say Respond, or press 1.");
-			gather.say("To repeat their question again, say Repeat, or press 2.");
-			gather.say(
-				"To hang up without recording a response, say Cancel, or press 3."
-			);
+			gather.say("To start recording your response, press 1.");
+			gather.say("To repeat their question again, press 2.");
+			gather.say("To hang up without recording a response, press 3.");
 			gather.pause({ length: 10 });
 
 			response.say("Sorry, we didn't receive any input. Goodbye!");
@@ -179,14 +170,8 @@ app.post("/recordAnswer", async (req, res) => {
 	if (req.body.Digits === undefined) {
 		req.body.Digits = 0;
 	}
-	if (req.body.SpeechResult === undefined) {
-		req.body.SpeechResult = "";
-	}
 
-	if (
-		req.body.Digits == "1" ||
-		req.body.SpeechResult.toLowerCase().includes("respond")
-	) {
+	if (req.body.Digits == "1") {
 		const callRef = db.collection("calls").doc(callSID);
 		const call = await callRef.get();
 		if (!call.exists) {
@@ -213,20 +198,14 @@ app.post("/recordAnswer", async (req, res) => {
 				res.send(twiml);
 			});
 		}
-	} else if (
-		req.body.Digits == "2" ||
-		req.body.SpeechResult.toLowerCase().includes("repeat")
-	) {
+	} else if (req.body.Digits == "2") {
 		const response = new VoiceResponse();
 		response.redirect({ method: "POST" }, "/askQuestion");
 
 		let twiml = response.toString();
 		res.header("Content-Type", "application/xml");
 		res.send(twiml);
-	} else if (
-		req.body.Digits == "3" ||
-		req.body.SpeechResult.toLowerCase().includes("cancel")
-	) {
+	} else if (req.body.Digits == "3") {
 		const callRef = db.collection("calls").doc(callSID);
 		const call = await callRef.get();
 		if (!call.exists) {
